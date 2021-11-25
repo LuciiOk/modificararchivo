918e56c907060fac31920464f2c899eb2d7dd96f
luciano portales <lportales655@gmail.com>
2021-11-24 22:38:53 -0300

    listo

--- Modificaarchivo.sh
+++ Modificaarchivo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ $# -ne 4 ]; then
+if [[ $# -ne 4 ]];then # se validan los parametros que se ingresaron al iniciar el sh
   echo El script no recibió los 4 parametros requeridos
   exit 1
 fi
@@ -10,27 +10,30 @@ opcion=$3
 
 echo $archivo
 
+#Verifica que el archivo que el archivo y el usuario del archivo exista.
 if (find / -user $dueno -iname $archivo -print -quit) then
     direccion=$(find / -user $dueno -iname $archivo -print -quit) #se almacena la ruta donde esta el
     echo $direccion                                                 #archvio
     if [ "$opcion" = "-p" ]; then # valida si el parametro es para los permisos
         chmod $4 "$direccion"
-        echo permiso
-        exit
+        echo los permisos del archivo \"direccion\" han sido cambiados a $4
+        exit 0 #al finalizar se termina el programa sin errores
     fi
     if [ "$opcion" = "-d" ]; then # valida si el parametro es para los duenos
         chown $4 "$direccion"
         echo "Ahora el dueno del archivo \"$direccion\" es $4"
-        exit
+        exit 0 #al finalizar se termina el programa sin errores
     fi
     if [ "$opcion" = "-g" ]; then # valida si el parametro es para los grupos
         chgrp $4 "$direccion"
         echo "Ahora el grupo del archivo \"$direccion\" es $4"
-        exit
+        exit 0 #al finalizar se termina el programa sin errores
     fi
-    echo Has ingresado una opcion incorrecta
+    echo Has ingresado una opcion incorrecta 
+    exit 1 #no se ingreso una opcion incorrecta con error
 else
-    echo el archivo ingresado no existe
+    echo El archivo ingresado no existe
+    exit 1 #se finaliza el programa
 fi
 
 
